997178d4f3a7363e54d5bb299d730b53eed82ab2
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-06-24 15:37:18 -0300

    Add files via upload

---
+++ canhao.js
@@ -0,0 +1,14 @@
+class Canhao {
+    constructor(x,y,width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.baseimg = loadImage("base.png");
+        this.canhaoimg = loadImage("canhao.png");
+    }
+    show(){
+        //atualizar as configurações de desenho de imagens
+        
+    }
+}
